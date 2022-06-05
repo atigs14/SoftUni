f04f18aefa598be7256a96d7d230e4a3645e3f01
Yasen Stoykov <atigs14@gmail.com>
2022-06-05 12:57:15 +0300

    Homework 04

--- JSAdvanced/_04_DomIntroduction/04. Search in List/search.js
+++ JSAdvanced/_04_DomIntroduction/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+   let listElements = document.querySelectorAll(`ul#towns li`);
+   let resultElement = document.getElementById(`result`);
+   let searchText = document.getElementById(`searchText`).value;
+   let matches = 0;
+
+   for (const iterator of listElements) {
+      iterator.style.fontWeight = `normal`;
+      iterator.style.textDecoration =- ``;
+   }
+
+   for (const iterator of listElements) {
+      let text = iterator.textContent;
+
+      if (text.match(searchText)) {
+         matches ++;
+         iterator.style.fontWeight = `bold`;
+         iterator.style.textDecoration = `underline`;
+      }
+   }
+
+   resultElement.textContent = `${matches} matches found`;
 }
